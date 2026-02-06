74f07917fa08a759d44c5b1f7238bcda06fceb17
LucioPg <lucio.di.capua@gmail.com>
2026-02-06 23:50:30 +0100

    added landing page to export

--- src/components/features/mod.rs
+++ src/components/features/mod.rs
@@ -3,9 +3,11 @@ pub mod dashboard;
 pub mod settings;
 pub mod logout;
 pub mod register_user;
+mod landing;
 
 pub use login::*;
 pub use logout::*;
 pub use dashboard::*;
 pub use settings::*;
 pub use register_user::*;
+pub use landing::*;
