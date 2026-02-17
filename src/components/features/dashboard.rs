2c0f62e0480e159673f2349818bf444d83357a7c
LucioPg <lucio.di.capua@gmail.com>
2026-02-17 15:53:41 +0100

    temp stored raw password e button

--- src/components/features/dashboard.rs
+++ src/components/features/dashboard.rs
@@ -1,3 +1,4 @@
+use crate::backend::password_types_helper::{PasswordScore, PasswordStrength, StoredRawPassword};
 use crate::components::StoredPasswordUpsertDialog;
 use crate::components::globals::{StatCard, StatVariant};
 use dioxus::prelude::*;
@@ -8,6 +9,19 @@ pub fn Dashboard() -> Element {
     let username = auth_state.get_username();
     let nav = use_navigator();
     let mut stored_password_dialog_open = use_signal(|| false);
+    #[allow(unused_mut)]
+    let mut current_stored_raw_password = use_signal(|| Option::<StoredRawPassword>::None);
+
+    let temp = StoredRawPassword {
+        id: Some(55),
+        user_id: 1,
+        location: "Google".to_string(),
+        password: "amazingPassword1256@ds!!".to_string().into(),
+        notes: Some("test".to_string()),
+        score: Some(PasswordScore::new(60)),
+    };
+    current_stored_raw_password.set(Some(temp));
+
     rsx! {
         div { class: "content-container animate-fade-in",
             div { class: "mb-8",
@@ -22,8 +36,23 @@ pub fn Dashboard() -> Element {
                 }
                 StatCard {
                     value: "0".to_string(),
+                    label: "God Passwords".to_string(),
+                    variant: StatVariant::Success,
+                }
+                StatCard {
+                    value: "0".to_string(),
+                    label: "Epic Passwords".to_string(),
+                    variant: StatVariant::Success,
+                }
+                StatCard {
+                    value: "0".to_string(),
                     label: "Strong Passwords".to_string(),
                     variant: StatVariant::Success,
+                }
+                StatCard {
+                    value: "0".to_string(),
+                    label: "Medium Passwords".to_string(),
+                    variant: StatVariant::Warning,
                 }
                 StatCard {
                     value: "0".to_string(),
@@ -34,11 +63,11 @@ pub fn Dashboard() -> Element {
             div { class: "card card-lg",
                 p { class: "text-body text-center", "Your passwords will appear here" }
             }
-            div { class: "card card-lg",
-                button { class: "btn btn-primary",
+            div { class: "card-no-border items-end",
+                button { class: "btn btn-success",
                     r#type: "button",
                     onclick: move |_| {stored_password_dialog_open.set(true);},
-                    "progress" }
+                    "New Password" }
             }
 
         }
@@ -46,7 +75,7 @@ pub fn Dashboard() -> Element {
             open: stored_password_dialog_open,
             on_confirm: move |_| {stored_password_dialog_open.set(false);},
             on_cancel: move |_| {stored_password_dialog_open.set(false);},
-            stored_raw_password: None,
+            stored_raw_password: current_stored_raw_password(),
         }
     }
 }
