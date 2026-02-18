18d5ba751e8e454506cc16accffd88ff0f325094
LucioPg <lucio.di.capua@gmail.com>
2026-02-18 02:44:21 +0100

    struc for password stats used in frontend

--- src/backend/password_types_helper.rs
+++ src/backend/password_types_helper.rs
@@ -337,6 +337,18 @@ pub enum PasswordStrength {
     GOD,
 }
 
+/// Enum per tenere traccia delle statistiche delle password ( usato nel frontend )
+#[derive(Default, Clone, Copy, Debug, PartialEq)]
+pub struct PasswordStats {
+    pub weak: usize,
+    pub medium: usize,
+    pub strong: usize,
+    pub epic: usize,
+    pub god: usize,
+    pub total: usize,
+    pub not_evaluated: usize,
+}
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, sqlx::Type)]
 #[sqlx(transparent)]
 pub struct PasswordScore(u8);
