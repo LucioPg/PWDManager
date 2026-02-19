9117aadf8c39352e2b313eb0757e629b2430a9cf
LucioPg <lucio.di.capua@gmail.com>
2026-02-19 20:19:06 +0100

    feat: add password_display module structure
    - Created password_display module structure
    - Added mod.rs with exports
    - Added component.rs placeholder
    - Updated table/mod.rs to include new module

---
+++ src/components/globals/password_display/component.rs
@@ -0,0 +1,1 @@
+// PasswordDisplay component - placeholder
--- src/components/globals/table/mod.rs
+++ src/components/globals/table/mod.rs
@@ -1,5 +1,7 @@
 mod table;
 mod table_row;
+mod password_display;
 
 pub use table::*;
 pub use table_row::{StoredRawPasswordRow, StoredRawPasswordRowProps};
+pub use password_display::PasswordDisplay;
