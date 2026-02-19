2bf538214e505733d19b4509275ce017fb6e6014
LucioPg <lucio.di.capua@gmail.com>
2026-02-19 20:48:50 +0100

    feat: add password_display module structure
    - Create src/components/globals/password_display/mod.rs
    - Create src/components/globals/password_display/component.rs
    - Export PasswordDisplay from globals/mod.rs

--- src/components/globals/password_display/component.rs
+++ src/components/globals/password_display/component.rs
@@ -1,1 +1,36 @@
-// PasswordDisplay component - placeholder
+use crate::components::globals::svgs::{EyeIcon, EyeOffIcon};
+use crate::components::globals::form_field::FormSecret;
+use dioxus::prelude::*;
+use secrecy::ExposeSecret;
+
+/// Componente PasswordDisplay - visualizza password con toggle visibility
+///
+/// # Esempio
+/// ```rust
+/// rsx! {
+///     PasswordDisplay {
+///         password: FormSecret(SecretString::new("my-password".into())),
+///         max_width: "200px".to_string(),
+///     }
+/// }
+/// ```
+#[component]
+pub fn PasswordDisplay(
+    /// La password da visualizzare (FormSecret per sicurezza)
+    password: FormSecret,
+    /// Classe CSS aggiuntiva per il container (opzionale)
+    #[props(default)]
+    class: Option<String>,
+    /// Larghezza massima del contenitore (default: 200px come in table_row)
+    #[props(default = "200px".to_string())]
+    max_width: String,
+    /// Callback quando si clicca sull'icona clipboard (TODO: implementare copia)
+    /// Se None, il button clipboard viene mostrato ma disabilitato
+    #[props(default)]
+    on_copy: Option<EventHandler<()>>,
+) -> Element {
+    // TODO: Implement component
+    rsx! {
+        div { "PasswordDisplay placeholder" }
+    }
+}
--- src/components/globals/table/mod.rs
+++ src/components/globals/table/mod.rs
@@ -1,7 +1,5 @@
 mod table;
 mod table_row;
-mod password_display;
 
 pub use table::*;
 pub use table_row::{StoredRawPasswordRow, StoredRawPasswordRowProps};
-pub use password_display::PasswordDisplay;
