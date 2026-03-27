2fc850c58e3c5673a7a161e8cc12a4eca25b3611
LucioPg <lucio.di.capua@gmail.com>
2026-03-27 15:35:31 +0100

    fix clippy warnings where clone can be avoided

--- src/components/globals/navbar.rs
+++ src/components/globals/navbar.rs
@@ -9,44 +9,51 @@ pub fn NavBar() -> Element {
     if auth_state.is_logged_in() {
         let avatar = auth_state.get_avatar();
         rsx! {
-        nav { id: "nav",
-                class: "navbar",
-            Link {to: Route::Dashboard,
-                class: "navbar-brand",
-                h3 { class: "navbar-brand-text", "Dashboard" }
-            }
-            div { id: "user-info",
-                    class: "navbar-user",
-            Link {to: Route::Settings, id: "settings",
-                        img {id: "little-avatar",
+            nav { id: "nav", class: "navbar",
+                Link { to: Route::Dashboard, class: "navbar-brand",
+                    h3 { class: "navbar-brand-text", "Dashboard" }
+                }
+                div { id: "user-info", class: "navbar-user",
+                    Link { to: Route::Settings, id: "settings",
+                        img {
+                            id: "little-avatar",
                             class: "avatar-md avatar-hover",
                             src: "{avatar}",
-                            alt: "User Avatar"
-            }
+                            alt: "User Avatar",
+                        }
                     }
-            Link {to: Route::Logout, id: "logout", class: "navbar-link text-error", "Logout"}
+                    Link {
+                        to: Route::Logout,
+                        id: "logout",
+                        class: "navbar-link text-error",
+                        "Logout"
+                    }
                 }
-        }
-        Outlet::<Route> {}
+            }
+            Outlet::<Route> {}
         }
     } else {
-        let nav_login = nav.clone();
-        let nav_register = nav.clone();
+        let nav_login = nav;
+        let nav_register = nav;
         rsx! {
-            nav { id: "nav",
-                class: "navbar",
+            nav { id: "nav", class: "navbar",
                 div { class: "nav-logo-container w-full h-full",
-                    Link {to: Route::LandingPage,
-                    class: "navbar-brand",
-                }
+                    Link { to: Route::LandingPage, class: "navbar-brand" }
                 }
 
                 div { class: "navbar-nav",
                     ActionButtons {
                         primary_text: "Login".to_string(),
                         secondary_text: "Register".to_string(),
-                        primary_on_click: move |_| { nav_login.push(Route::Login); },
-                        secondary_on_click: move |_| { nav_register.push(Route::UpsertUser {user_to_edit: auth_state.get_user()}); },
+                        primary_on_click: move |_| {
+                            nav_login.push(Route::Login);
+                        },
+                        secondary_on_click: move |_| {
+                            nav_register
+                                .push(Route::UpsertUser {
+                                    user_to_edit: auth_state.get_user(),
+                                });
+                        },
                         variant: ActionButtonsVariant::Nav,
                     }
                 }
